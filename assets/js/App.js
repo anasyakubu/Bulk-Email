24ae05ce37cfbba34a7f3640f41f723985c28987
anasyakubu <yakubuanas04@gmail.com>
2023-06-15 03:38:58 +0100

    Added single send functions

--- assets/js/App.js
+++ assets/js/App.js
@@ -1,52 +1,87 @@
 // NOT AVAILABLE IN YOUR COUNTRY SCRIPT
-const notAvailable = document.querySelectorAll(".notAvailable");
-notAvailable.addEventListener('click', (e) => {
-  e.preventDefault();
-  alert("NYM.JS is not Available in Your Country")
-})
+function notAvailable() {
+  alert("NYM.JS is not Available in Your Country");
+}
 
-const emails = ['yakubuanas04@gmail.com', 'yakubuabbas04@gmail.com', 'unaeesunaees@gmail.com'];
-const send = document.querySelector('.send');
+// SEND SINGLE EMAIL FUNCTIONS
+function single() {
+  // alert("HELLO");
 
-send.addEventListener('click', (e) => {
-  e.preventDefault();
-  alert('Working');
-  //alert(emails);
+  const singleTo = document.querySelector(".singleTo").value;
+  const singleBody = document.querySelector(".singleBody").value;
+  const singleForm = document.querySelector(".singleForm");
 
-  emails.forEach(emails => {
+  if (singleTo == "" || singleBody == "") {
+    alert("Input Field Required");
+  } else {
     Email.send({
       SecureToken: "864e6dbb-9d31-47ca-8af1-4371d598e59f",
-      To: emails,
+      To: singleTo,
       From: 'yakubuanas04@gmail.com',
-      Subject: "Bulk Email",
-      Body: "<h3>This is a Bulk Email</h3>" +
+      Subject: "NYM Emailing System (Single)",
+      Body: "<h3>" + singleBody + "</h3>" +
         "<h6>Thank You..</h6>",
     }).then(
-      message => alert(message),
-      alert("Sent")
+      message => alert(message + "Check your Email"),
+      singleForm.reset()
+      // alert("Sent")
     );
-  })
+  }
+}
 
-});
+// SEND MULTIPLE EMAIL
+
+
+// const multipleTo = document.querySelector(".multipleTo").value;
+const multipleBody = document.querySelector(".multipleBody").value;
+const multipleForm = document.querySelector(".multipleForm");
 
 // Define an empty array
 var myArray = [];
-
 function addToArray() {
   // Get the value from the input field
-  var inputValue = document.getElementById("inputField").value;
+  var multipleTo = document.querySelector(".multipleTo").value;
 
   // Check if the array has reached the limit
-  if (myArray.length >= 10) {
+  if (myArray.length >= 3) {
     console.log("Array limit reached");
     return; // Exit the function if the limit is reached
   }
 
   // Add the value to the array
-  myArray.push(inputValue);
+  myArray.push(multipleTo);
 
   // Optional: Display the array
-  const result = document.querySelector('.result');
+  const result = document.querySelector('small');
   result.innerHTML = myArray;
   console.log(myArray);
+
+  function multiple() {
+    alert("Working");
+    console.log(Array);
+  }
+
 }
+
+
+
+
+// myArray.forEach(emails => {
+//   Email.send({
+//     SecureToken: "864e6dbb-9d31-47ca-8af1-4371d598e59f",
+//     To: emails,
+//     From: 'yakubuanas04@gmail.com',
+//     Subject: "NYM Emailing System (Multiple)",
+//     Body: "<h3>" + multipleBody + "</h3>" +
+//       "<h6>Thank You..</h6>",
+//   }).then(
+//     message => alert(message + "Check your Email"),
+//     multipleForm.reset(),
+//     // alert("Sent")
+//   );
+// });
+
+
+// const emails = ['yakubuanas04@gmail.com', 'yakubuabbas04@gmail.com', 'unaeesunaees@gmail.com'];
+// const send = document.querySelector('.send');
+
